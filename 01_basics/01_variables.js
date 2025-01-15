dd3ec2f7ab8b531dad7430e20159a2b61eefeb72
RAMA <76054265+GitAmarHub@users.noreply.github.com>
2025-01-15 16:53:32 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 210210
+let accountEmail = "name@gmail.com"
+let accountPassword = "0987"
+let accountState = "positive"
+
+accountEmail = "Bhartu@akuu.com"
+accountPassword = "murgijaved"
+
+console.log(accountID, accountEmail, accountPassword);
+console.table([accountID, accountEmail, accountPassword,accountState]);
+//console.log(x); // Outputs: undefined
+//var x = 10;
+//console.log(y); // Throws ReferenceError: Cannot access 'y' before initialization
+//let y = 20; 
