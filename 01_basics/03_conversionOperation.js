9d473327af286eee6cefcc747f323871a4a57376
RAMA <76054265+GitAmarHub@users.noreply.github.com>
2025-01-16 18:49:34 +0000

    Conversion Operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let age= true;
+
+console.log(typeof age);
+console.log(age);
+
+//"25" = 25
+// "25abc" = NAN 
+
+
+let x = NaN;
+console.log(typeof x);
+//let isLoggedIn = 0
+
+//let booleanIsLoggesdIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggesdIn);
