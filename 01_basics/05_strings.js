3efcc0526b046a5599ac9879a7078c7ea4969056
RAMA <76054265+GitAmarHub@users.noreply.github.com>
2025-01-23 16:56:28 +0000

    all new updates 04, 05, data summary

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,19 @@
+ const gameName = new String('Akashmilfs');
+ // const gameName = new String('Akashmilfs');   //Enter
+ //gameName
+ //it will all strings methods in inspect console
+
+ //Mdn Web docs //https://developer.mozilla.org/en-US/docs/Web/JavaScript
+ console.log(gameName.charAt('3'));
+ console.log(gameName.indexOf('m'));
+ 
+ //total methods in string spend some time 15 to 30 min
+
+ const newStringOne = "   Akshmilk";
+ console.log(newStringOne);
+ console.log(newStringOne.trim());
+
+ const url = "https://hitesh.com" 
+
+ 
+
---
+++ 01_basics/Notes.js
@@ -0,0 +1,2 @@
+https://github.com/hiteshchoudhary/js-hindi-youtube
+//github repo chai or code
