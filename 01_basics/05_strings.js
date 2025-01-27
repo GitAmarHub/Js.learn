878928b0c904edd88234f3debbd0b86b0926edb3
RAMA <76054265+GitAmarHub@users.noreply.github.com>
2025-01-27 15:58:07 +0000

    num and maths

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,11 +1,17 @@
- const gameName = new String('Akashmilfs');
+const name = "hitesh"
+const repoCount = 50
+
+console.log(name + repoCount + " String(value)"); //space under " " work as a space
+
+const gameName = new String('Akashmilks');
  // const gameName = new String('Akashmilfs');   //Enter
  //gameName
- //it will all strings methods in inspect console
+ //it will show all strings methods in inspect console
 
  //Mdn Web docs //https://developer.mozilla.org/en-US/docs/Web/JavaScript
- console.log(gameName.charAt('3'));
- console.log(gameName.indexOf('m'));
+ console.log(gameName.charAt('3'));  // to see character on that index
+ console.log(gameName.indexOf('m')); // to see index of that character 
+
  
  //total methods in string spend some time 15 to 30 min
 
